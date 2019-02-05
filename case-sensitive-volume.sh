452bf1dfbf3472f219aaa8c79bedfa6624869e4b
Charles Peterson <artistan@gmail.com>
2019-02-04 20:35:34 -0600

    create and mount case sensitive volume

--- case-sensitive-volume.sh
+++ case-sensitive-volume.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
 
 echo 'Mount Point / Directory Name'
-echo "/Volumes/Macintosh\ HD/Users/${USER}/{directory_name}"
+echo "/Users/${USER}/{directory_name}"
 read -p '{directory_name}: ' directory_name
-if [ ! ${directory_name} ]; then
+if [[ ! "${directory_name}" ]]; then
     echo "Please provide apfs drive volume name."
     exit;
 fi
 
+NEW_DIR="/Users/${USER}/${directory_name}"
+if [[ -d  "${NEW_DIR}" ]]; then
+    echo "Directory Already Exists;"
+    exit;
+fi
+echo "${NEW_DIR}";
+
 echo "Please provide apfs drive volume size (ex. 10G)."
 read -p '{volume_size}: ' volume_size
-if [ ! ${volume_size} ]; then
+if [[ ! "${volume_size}" ]]; then
     echo "Invalid volume size."
     exit;
 fi
 
 echo "Please provide apfs drive container size (ex. disk1)."
 read -p '{drive_container}: ' drive_container
-if [ ! ${drive_container} ]; then
+if [[ ! "${drive_container}" ]]; then
     echo "Invalid drive container."
     exit;
 fi
 
-NEW_DIR="/Volumes/Macintosh\ HD/Users/${USER}/${directory_name}"
-if [ ! -d ${NEW_DIR} ]; then
-    mkdir ${NEW_DIR}
-fi
+# make the directory to mount to...
+mkdir "${NEW_DIR}"
 # make code sensitive mounted apfs volume
 # diskutil … https://eclecticlight.co/2017/04/08/how-to-make-your-own-apfs-volume/
 # mount point …. https://apple.stackexchange.com/questions/309861/mount-apfs-volume-to-users-with-the-disk-utility-during-the-installation
-sudo diskutil apfs addVolume ${drive_container} "Case-sensitive APFS" ${directory_name} -reserve ${volume_size} -mountpoint ${NEW_DIR}
+sudo diskutil apfs addVolume ${drive_container} "Case-sensitive APFS" "${directory_name}" -reserve "${volume_size}" -mountpoint "${NEW_DIR}"
 # go into that mount
-cd /Users/${USER}
-sudo chown ${USER} :staff ${directory_name}
+cd "/Users/${USER}"
+sudo chown "${USER}":staff ${directory_name}
 cd ${directory_name}
